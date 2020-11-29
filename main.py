e8e3a2a65e4f04d0cc3e5d637eacb21dbb51f03e
OsamaAlOlabi <osamaolabi@hotmail.com>
2020-11-30 00:49:45 +0100

    1- Fixed some events 2- Finished up the game

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ sMMMMMMMMMh-                                   sMMMMMMMMN-
 *******************************************************************************
 ''')
 
+
 # Highlighting Important Messages
 def border_msg(msg):
     count = len(msg) + 2  # dash will need +2 too
@@ -68,28 +69,29 @@ def border_msg(msg):
 user_name = input("What is your name?\n")
 
 # Welcome Message
-welcome_message = print(f"Welcome to hell lord {user_name}.\nThere is one rule here, SURVIVE.")
+print(f"Welcome to hell lord {user_name}.\nThere is one rule here, SURVIVE.")
 
 # Keep track if the game ended or not (Game still on = True, Game ended = False)
 game_state = True
 
+
 # Main game code in here
 def first_assignment():
-
     global game_state
 
     # if game state is TRUE keep playing, if FALSE stop the game
     while game_state == True:
 
-        event1 = print('''\nYou have just arrived to the Devil's facility in AREA 51. 
+        print('''\nYou have just arrived to the Devil's facility in AREA 51. 
 What do you want to do?:
 (1)- Walk inside
-(2)- Run away and abandon the training''')
+(2)- Run away and leave hell
+''')
         user_input = input("> ")
 
         if user_input == "1":
             while True:
-                event2 = print('''
+                print('''
 You open the door to see a locked gate infront of you and you find a sword laid down on the floor.
 You notice a note near the sword that says "This cursed sword, is forged from the devil himself.
 What do you do?:
@@ -109,53 +111,50 @@ What do you do?:
 ''')
                         user_input = input("> ")
                         if user_input == "1":
-                            print("You have slain every demon in Hell.\nCongratiolations you are the DEVIL")
+                            print("You have slain every demon in Hell.\nCongratiolations you are now the DEVIL")
                             game_state = False
                             repeat()
                             break
                         elif user_input == "2":
-                            print("Did you really thought you could outrun demons?. A demon has picked up the sword and thrusted it through your heart."
-                                  "\nThe demon got a huge promotion and became the DEVIL")
+                            print(
+                                "Did you really think you could outrun demons?. A demon has picked up the sword and "
+                                "thrusted it through your heart. "
+                                "\nThe demon got a huge promotion and became the DEVIL.\n")
                             game_state = False
                             repeat()
                             break
                         else:
                             print("Invalid choice. Please type a number from the list")
                 elif user_input == "2":
-                    print("The wolves heard you and ate you.")
+                    while True:
 
-                    break
-                else:
-                    print("Invalid choice. Please type a number from the list")
+                        print(
+                            "You walk by the sword without picking it up, fighting every urge in your body to do so.\n"
+                            "You reach the gate and you knock 3 times. The gate opens unleashing a horde of demons.\n"
+                            "You try to run away, but it's too late, they devour your soul.\n")
+                        game_state = False
+                        repeat()
+                        break
+                    else:
+                        print("Invalid choice. Please type a number from the list")
 
-
-        elif event1 == "2":
-            while True:
-                event2 = input('You\' reached a frozen lake. There is an island at the end of the lake. Type "wait" '
-                                'to wait for a boat. Type "walk" to walk across.\n').lower()
-                if event2 == "wait":
-                    print("while waiting your hands got frozen, which led to your feet to freeze which was the result "
-                          "of your death")
-
-                    break
-                elif event2 == "walk":
-                    print("The ice cracked while you were walking, you fell into the cold water, and get eaten by a shark.")
-
-                    break
-                else:
-                    print("Invalid choice. Please type something from the list")
-
-        else:
-            print("Invalid choice. Please type something from the list")
+        elif user_input == "2":
+            print('Dang, no one ever has thought of doing that!\n'
+                  'Congratiolations, you have escaped hell\n'
+                  'Oh god, you should\'ve seen the look on your face.\n'
+                  'ofcourse you didn\'t escape, no one ever does.\n'
+                  'You tripped and fell on your head\n'
+                  'You DIED\n')
+            game_state = False
+            repeat()
 
 
 def repeat():
-
     global game_state
 
     while game_state == False:
 
-        play_again = border_msg('The game has ended. Would you like to play again? "Y" or "N"')
+        border_msg('The game has ended. Would you like to play again? "Y" or "N"')
 
         user_input = input("> ")
 
