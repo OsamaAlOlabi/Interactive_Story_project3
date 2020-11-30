fdb1bf1d373244ca6989aa8444f913e50b53d591
OsamaAlOlabi <osamaolabi@hotmail.com>
2020-11-30 10:29:40 +0100

    1 - created a dictionary for choices 2 - created a dictionary for notes 3 - changes main function name to welcome_to_hell 4 - changed picture to a little devil

--- main.py
+++ main.py
@@ -1,56 +1,59 @@
 print('''
 *******************************************************************************
-                               `...`                                                            
-                               :sdNMMMMMmy/`                                                        
-                             :mMMMMMMMMMMMMNo`                                                      
-                            oMMMMMMMMMMMMMMMMd`                                                     
-                           /MMMMMMMMMMMMMMMMMMh                                                     
-                           hMMMMMMMMMMMMMMMMMMM`                                                    
-                           yMMMMMMMMMMMMMMMMMMM`                                                    
-                           -MMMMMMMMMMMMMMMMMMs                                                     
-                            /NMMMMMMMMMMMMMMMy`                                                     
-                             -yNMMMMMMMMMMMNo-....................................................` 
-                               ./ydmmMMMMMMNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNm+
-                          `-://:-.`.hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-                        :ymNMMMMMmhsMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNmmmmmmmmmd+
-                      `sMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM/`````````` 
-                      sMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmo            
-                     -MMMMMMMMMMMMMMMMssyhhhhhhhmMMMMMMMMMMMMMmhhhhmMMMMMMMMMMMMMMMds/.             
-                     yMMMMMMMMMMMMMMMMy.      `/mMMMMMMMMMMMMd-  `+mMMMMMMMMMMMMMd/`                
-                    -MMMMMMMMMMMMMMMMMMNs.   -hMMMMMMMMMMMMNo` .+mMMMMMMMMMMMMMd/`                  
-                    hMMMMMMMMMMMMMMMMMMMMNs:oNMMMMMMMMMMMMMmo:oNMMMMMMMMMMMMMm+`                    
-                   :MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd+`                      
-                   dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd/                         
-                  /MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm/                           
-                  mMMMMMMMMMMMMMMMNdMMMMMMMMMMMMMMMdhNMMMMMMMMMMMMMMMd/                             
-                 +MMMMMMMMMMMMMMMMo -ohNMMMMMMMNho-  `:ohNMMMMMMMNho-                               
-                `NMMMMMMMMMMMMMMMN`     `--::-`           `--:--.                                   
-                oMMMMMMMMMMMMMMMM+                                                                  
-               `NMMMMMMMMMMMMMMMm                                                                   
-               oMMMMMMMMMMMMMMMM/                                                                   
-               NMMMMMMMMMMMMMMMd                                                                    
-               NMMMMMMMMMMMMMMMh.                                                                   
-               NMMMMMMMMMMMMMMMMNh:                                                                 
-              `MMMMMMMMMMMMMMMMMMMMm+`                                                              
-              :MMMMMMMMMMMMMMMMMMMMMMmo`                                                            
-              sMMMMMMMMMMMMMMMMMMMMMMMMm+`                                                          
-             `mMMMMMMMMMM/yNMMMMMMMMMMMMMm+`                                                        
-             +MMMMMMMMMMd  .+mMMMMMMMMMMMMMd:                                                       
-            .mMMMMMMMMMM/    `+dMMMMMMMMMMMMNs.                                                     
-            yMMMMMMMMMMd       `/mMMMMMMMMMMMMm/                                                    
-           oMMMMMMMMMMN:         `+mMMMMMMMMMMMMs`                                                  
-          oMMMMMMMMMMMo            .yNMMMMMMMMMMMm-                                                 
-        `sMMMMMMMMMMMy               :dMMMMMMMMMMMN/                                                
-       .hMMMMMMMMMMMh`                `sNMMMMMMMMMMMs`                                              
-      /mMMMMMMMMMMMh`                   :mMMMMMMMMMMMy`                                             
-    .hMMMMMMMMMMMMy`                     .dMMMMMMMMMMMh`                                            
-  .sNMMMMMMMMMMMN+                        `yMMMMMMMMMMMh`                                           
-`sNMMMMMMMMMMMMd-                           sMMMMMMMMMMMy`                                          
-dMMMMMMMMMMMMNo`                             oMMMMMMMMMMMo                                          
-MMMMMMMMMMMMy-                                oMMMMMMMMMMh                                          
-sMMMMMMMMMh-                                   sMMMMMMMMN-                                          
- /hNMMMNy-                                      -ymMMNdo`                                           
-    ..`                                                                                  
+                                       .hMM                 NMh.                                    
+                                      -NMMM.               `MMMN:                                   
+                                     `NMMMMy               yMMMMN`                                  
+                                     +MMMMMMs-:/+ossso+/:-sMMMMMM+                                  
+                                     oMMMMMMMNMMMMMMMMMMMNMMMMMMMo                                  
+                                     -MMMMMMMMMMMMMMMMMMMMMMMMMMM:                                  
+                                    -hMMMMMMMMMMMMMMMMMMMMMMMMMMMh-                                 
+                                  `sNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNs`                               
+                                 .dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd.                              
+                                `dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm`                             
+                                yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy                             
+                               -MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM-          o                 
+                               oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo         oM.                
+                               sMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy        oMM:         `      
+                               oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs      `oMMN+     `-/y/      
+                               -MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM:     `yMMy-:  `/sdNMs       
+                                hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd     `hMMs`   `sMMMMo        
+                                .mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN-    .dMMo   `/dMMmho         
+                                 -mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm-    `mMM+  `/dMMmo`           
+                                  .hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh.     /MMN `/dMMmo`   `+yysso+/.
+                                    /mMMMMMMMMMMMMMMMMMMMMMMMMMMMm/       .NMMsdMMNo`  `./ymMMNh+-` 
+                                     `/hNMMMMMMMMMMMMMMMMMMMMMNd/`         /MMMMMs.``-ohNMNdo:`     
+                                        .+hMMMMMMMMMMMMMMMMMh+.          `:dMMNMMNmmNMMNy/.         
+                                        .smMMMMMMMMMMMMMMMMMms-         :dMMNo./shddho:`            
+                                      :hMMMMMMMMMMMMMMMMMMMMMMMh:     :dMMNo`                       
+                                    .hMMMMMMMMMMMMMMMMMMMMMMMMMMMh. :dMMNs`                         
+                                   +MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMdMMNs`                           
+                                 `yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs`                             
+              `                 `hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd`                             
+      `.:+shmN-                 hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh                             
+    /dNNMMMMMm                 oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo                            
+     .dMMMMMMo                .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM-                           
+    :dMMNdMMM.                yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh                           
+   sMMNo``-sh                .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.                          
+  sMMm-     `                +MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo                          
+ +MMN-                       hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh                          
+`NMM+                        dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm                          
++MMm                         mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN                          
+dMMs                         NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN                          
+NMM/                         MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM`                         
+MMM:                        .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.                         
+NMM/                        :MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM:                         
+hMMs                        +MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM+                         
+/MMN`                       oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMs                         
+`mMMs                       sMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy                         
+ :NMM+                      sMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy                         
+  /NMMo`                    +MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMo                         
+   -mMMd/`               `.:yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM-                         
+    `oNMMmy/-````````-/oydNMMNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh                          
+      `/hNMMMNmmdmmNMMMMNNho:-dMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMd.                          
+         `:+syhdddhhyo+:.     `oNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNo`                           
+                                `/hNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNh+`                             
+                                    -/shmNMMMMMMMMMMMMMMMMMMMNmhs+-`                                
+                                          `.-://+++++++//:-.`                     
 *******************************************************************************
 ''')
 
@@ -74,83 +77,96 @@ print(f"Welcome to hell lord {user_name}.\nThere is one rule here, SURVIVE.")
 # Keep track if the game ended or not (Game still on = True, Game ended = False)
 game_state = True
 
+# Game notes put in a dictionary
+event_notes = { 0: "Invalid choice. Please type a number from the list",
+
+                1: 'Dang, no one ever has thought of doing that!\n'
+                    'Congratiolations, you have escaped hell.\n'
+                    'Oh god, you should\'ve seen the look on your face.\n'
+                    'ofcourse you didn\'t escape, no one ever does.\n'
+                    'You tripped and fell on your head.\n'
+                    'You DIED!\n',
+
+               2: "You have slain every demon in Hell.\nCongratiolations you are now the DEVIL",
+
+               3: "Did you really think you could outrun demons?. A demon has picked up the sword and "
+                    "thrusted it through your heart. "
+                    "\nThe demon got a huge promotion and became the DEVIL.\n",
+
+                4:  "You walk by the sword without picking it up, fighting every urge in your body to do so.\n"
+                    "You reach the gate and you knock 3 times. The gate opens unleashing a horde of demons.\n"
+                    "You try to run away, but it's too late, they devour your soul.\n"}
+
+# Game choices put in a dictionary
+event_choices = {1: "\nYou have just arrived to the Devil's facility in AREA 51.\n"
+                    "What do you want to do?:\n"
+                    "(1)- Walk inside\n(2)- Run away and leave hell",
+
+                 2: "You open the door to see a locked gate infront of you and you find a sword laid down on the floor.\n"
+                    "You notice a note near the sword that says \"This cursed sword, is forged from the devil himself.\"\n"
+                    "What do you do?:\n"
+                    "(1)- Pick up the sword\n"
+                    "(2)- Knock on the locked gate",
+
+                 3: "Once you pick it up you feel your whole body is being filled with power, dark power.\n"
+                    f"The gate opens, and a hundreds of demons start running towards you screaming lord {user_name}.\n"
+                    "What do you do?:\n"
+                    "(1)- Fight them all\n"
+                    "(2)- Throw the sword away and run."}
+
 
 # Main game code in here
-def first_assignment():
+def welcome_to_hell():
+    global event_choices
+    global  event_notes
     global game_state
+
 
     # if game state is TRUE keep playing, if FALSE stop the game
     while game_state == True:
 
-        print('''\nYou have just arrived to the Devil's facility in AREA 51. 
-What do you want to do?:
-(1)- Walk inside
-(2)- Run away and leave hell
-''')
+        print(event_choices[1])
         user_input = input("> ")
 
         if user_input == "1":
             while True:
-                print('''
-You open the door to see a locked gate infront of you and you find a sword laid down on the floor.
-You notice a note near the sword that says "This cursed sword, is forged from the devil himself.
-What do you do?:
-(1)- Pick up the sword
-(2)- Knock on the locked gate
-''')
+                print(event_choices[2])
                 user_input = input("> ")
 
                 if user_input == "1":
                     while True:
-                        print(f'''
-Once you pick it up you feel your whole body is being filled with power, dark power.
-The gate opens, and a hundreds of demons start running towards you screaming lord {user_name}.
-What do you do?:
-(1)- Fight them all
-(2)- Throw the sword away and run.
-''')
+                        print(event_choices[3])
                         user_input = input("> ")
                         if user_input == "1":
-                            print("You have slain every demon in Hell.\nCongratiolations you are now the DEVIL")
+                            print(event_notes[2])
                             game_state = False
                             repeat()
                             break
                         elif user_input == "2":
-                            print(
-                                "Did you really think you could outrun demons?. A demon has picked up the sword and "
-                                "thrusted it through your heart. "
-                                "\nThe demon got a huge promotion and became the DEVIL.\n")
+                            print(event_notes[3])
                             game_state = False
                             repeat()
                             break
                         else:
-                            print("Invalid choice. Please type a number from the list")
+                            print(event_notes[0])
+
                 elif user_input == "2":
                     while True:
-
-                        print(
-                            "You walk by the sword without picking it up, fighting every urge in your body to do so.\n"
-                            "You reach the gate and you knock 3 times. The gate opens unleashing a horde of demons.\n"
-                            "You try to run away, but it's too late, they devour your soul.\n")
+                        print(event_notes[4])
                         game_state = False
                         repeat()
                         break
                 else:
-                    print("Invalid choice. Please type a number from the list")
+                    print(event_notes[0])
 
         elif user_input == "2":
-            print('Dang, no one ever has thought of doing that!\n'
-                  'Congratiolations, you have escaped hell\n'
-                  'Oh god, you should\'ve seen the look on your face.\n'
-                  'ofcourse you didn\'t escape, no one ever does.\n'
-                  'You tripped and fell on your head\n'
-                  'You DIED\n')
+            print(event_notes[1])
             game_state = False
             repeat()
         else:
-            print("Invalid choice. Please type a number from the list")
+            print(event_notes[0])
 
-
+# MARK: - Game Repeat Function
 def repeat():
     global game_state
 
@@ -162,7 +178,7 @@ def repeat():
 
         if user_input == "y":
             game_state = True
-            first_assignment()
+            welcome_to_hell()
         elif user_input == "n":
             print("Game Over")
             exit()
@@ -171,4 +187,4 @@ def repeat():
             print("Choose Y or N")
 
 
-first_assignment()
+welcome_to_hell()
